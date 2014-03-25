dfb2f5bc7be387f23edded867d723bd09555ea1b
Andrew Dean <andrew@xvt.com.au>
2014-03-25 14:59:54 +1100

    Can now specify zip file path when using export command

--- ckanext/dsactions/commands/export.py
+++ ckanext/dsactions/commands/export.py
@@ -1,9 +1,14 @@
 from ckan.lib.cli import CkanCommand
 import ckan
+import sys
+import shutil
 from ckanext.dsactions.export import exportPackages
 
 class ExportCommand(CkanCommand):
     '''
+    Exports all the datasets to a zip file
+    
+    export FILE_PATH - dumps the datsets to the given zip file
     '''
     summary = __doc__.split('\n')[0]
     usage = __doc__
@@ -17,9 +22,14 @@ class ExportCommand(CkanCommand):
     def command(self):
         self._load_config()
         
+        if len(self.args) == 0:
+            self.parser.print_usage()
+            sys.exit(1)
+        
         query = ckan.model.Session.query(ckan.model.Package)
         file_zip_path = exportPackages(query)
         print file_zip_path
-        
+        shutil.move(file_zip_path, self.args[0])
+        print 'done'
         
         
